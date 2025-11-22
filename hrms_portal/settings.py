cf9843a8be8ba9313c1b62d5bd9e321fc261d81f
Priyanshua <priyanshus@dewsolutions.in>
2025-11-22 22:05:25 +0530

    debugs

--- hrms_portal/settings.py
+++ hrms_portal/settings.py
@@ -133,6 +133,7 @@ TIME_ZONE = 'Asia/Kolkata'
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
